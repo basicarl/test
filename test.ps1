edc602458019e4b436b58fd386158a4472af12ef
gustawinho <gustawo@outlook.com>
2023-08-26 13:45:04 +0200

    commit 2

--- test.ps1
+++ test.ps1
@@ -1,1 +1,9 @@
 Write-Host 'ala ma kota' -ForegroundColor Magenta
+
+@'
+tree 7b0e631fa594de3df3135ceaf80f93820da06ebc
+author gustawinho <gustawo@outlook.com> 1693048407 +0200
+committer gustawinho <gustawo@outlook.com> 1693048407 +0200
+
+commit 1
+'@ | git hash-object --stdin
